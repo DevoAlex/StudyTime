5472426bbc3ceaf7464f48e094b00a4eeefee412
DevoAlex <97481834+DevoAlex@users.noreply.github.com>
2022-12-05 18:20:06 +0100

    Spinner css

--- Client/src/components/LoadingSpinner.style.jsx
+++ Client/src/components/LoadingSpinner.style.jsx
@@ -14,8 +14,8 @@ export const Spinner = styled.div`
   margin-bottom: 2rem;
   width: 3rem;
   height: 3rem;
-  border: 0.8rem dotted #2d323e;
-  border-top: 0.8rem dotted #87cefa;
+  border: 0.8rem double #2d323e;
+  border-top: 0.8rem double #87cefa;
   border-radius: 50%;
   animation: spinner 1.5s linear infinite;
   @media ${device.tablet} {
@@ -27,14 +27,14 @@ export const Spinner = styled.div`
   @media ${device.laptopL} {
     width: 8rem;
     height: 8rem;
-    border: 2rem dotted #2d323e;
-    border-top: 2rem dotted #87cefa;
+    border: 2rem double #2d323e;
+    border-top: 2rem double #87cefa;
   }
   @media ${device.desktop} {
     width: 10rem;
     height: 10rem;
-    border: 2.5rem dotted #2d323e;
-    border-top: 2.5rem dotted #87cefa;
+    border: 2.5rem double #2d323e;
+    border-top: 2.5rem double #87cefa;
   }
 `;
 
